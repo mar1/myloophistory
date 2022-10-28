926ab3c83e713657d33d5226bb87d890f875986e
Marin <51886896+mar1@users.noreply.github.com>
2022-10-28 17:05:19 +0100

    Basic frontend + logo

--- frontend/src/views/Tab1Page.vue
+++ frontend/src/views/Tab1Page.vue
@@ -2,28 +2,35 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title><img src="/assets/logo.png" id="logo" class="breath"></ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
+
     
-      <ExploreContainer name="Tab 1 page" />
+    <ion-card id="mainCard">
+    <ion-card-header>
+      <ion-card-title><b style="font-size: 130%;">MyLoopStory</b><br>is a dynamic video NFT-based decentralized social network</ion-card-title>
+      <ion-card-subtitle>Build your Loop with 3 seconds of video, follow your frens & make new ones.</ion-card-subtitle>
+      <ion-card-subtitle><b>Record you life as an NFT and enjoy forever your full ownership over it !</b></ion-card-subtitle>
+    </ion-card-header>
+    <ion-card-content>
+    </ion-card-content>
+    </ion-card>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/vue';
 
 export default  defineComponent({
   name: 'Tab1Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle },
 });
 </script>
+
+<style>
+@import '../../public/assets/style.css';
+</style>
